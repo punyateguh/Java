96a491d810e268b97cc75d5d55c1ed57a7bdaa83
punyateguh <42632106+punyateguh@users.noreply.github.com>
2019-11-16 00:41:40 -0800

    Scanner

---
+++ Scanner_aritmatika.java
@@ -0,0 +1,15 @@
+package com.tutorial;
+
+import java.util.Scanner;
+
+public class tutorial {
+
+	public static void main(String[] args) {
+		
+		Scanner userInput = new Scanner(System.in);
+		
+		int input = userInput.nextInt();
+		System.out.println("Hasil = " + input);
+	}
+
+}

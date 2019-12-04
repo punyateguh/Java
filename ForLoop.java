f3180d0f5d27cf1552c9c23741f92a3889c644d4
punyateguh <42632106+punyateguh@users.noreply.github.com>
2019-12-04 23:24:20 +0700

    For loop

---
+++ ForLoop.java
@@ -0,0 +1,19 @@
+public class ForLoop{
+
+    public static void main(String[]args){
+
+    // for (inisialisasi; kondisi; stop nilai){
+
+    //     aksi;
+
+    // }
+        
+    
+        for(int a=0; a <= 10; a++){
+            
+            System.out.println("Nilai loop ke - " + a);
+            
+        }
+
+    }
+}
